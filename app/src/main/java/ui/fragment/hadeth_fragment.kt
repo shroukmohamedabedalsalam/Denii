a386af196988ce0210cebef6d2f43e7074b86546
Shosho <shroukmohammed332211@gmail.com>
2021-03-22 22:09:34 +0200

    quoran complete

---
+++ app/src/main/java/ui/fragment/hadeth_fragment.kt
@@ -0,0 +1,27 @@
+package ui.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.denii.R
+
+
+class hadeth_fragment : Fragment() {
+
+
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_hadeth_fragment, container, false)
+    }
+
+
+                }
+
+
