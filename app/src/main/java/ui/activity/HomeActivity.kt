c1087eb59cc469be8268d5081cfea1679c2d4e0a
Shosho <shroukmohammed332211@gmail.com>
2021-03-31 00:32:23 +0200

    tasbe7 complete && qouran

--- app/src/main/java/ui/activity/HomeActivity.kt
+++ app/src/main/java/ui/activity/HomeActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+
 import com.example.denii.R
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.activity_home.*
--- app/src/main/java/ui/fragment/tasbeh_fragment.kt
+++ app/src/main/java/ui/fragment/tasbeh_fragment.kt
@@ -1,24 +1,45 @@
 package ui.fragment
 
 import android.os.Bundle
+
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
 import com.example.denii.R
+import kotlinx.android.synthetic.main.fragment_tasbeh_fragment.*
 
-class tasbeh_fragment : Fragment() {
+class tasbeh_fragment() : Fragment() {
 
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_tasbeh_fragment, container, false)
     }
+    var count = 0
+
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        tsbeha .setOnClickListener(View.OnClickListener {
+            count++
+            tasb7nmber.setText("" + count).toString()
+            val totalcount = count
+            totalNumber.setText("" + totalcount).toString()
+
+})
+    refrach.setOnClickListener(View.OnClickListener {
+        tasb7nmber.setText("")
+
+    })
+
 
 
     }
-
+}
